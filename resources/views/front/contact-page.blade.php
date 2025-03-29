08a87574e13151756bc66da82349c8755469817b
Kram-V <markanthonyvivar24@gmail.com>
2025-03-29 17:18:05 +0800

    modified styling in contact page

--- resources/views/front/contact-page.blade.php
+++ resources/views/front/contact-page.blade.php
@@ -17,11 +17,11 @@
   </div>
 </div>
 
-<div class="contact pt_70">
+<div class="contact pt_70 pb_70"> 
   <div class="container">
       <div class="row">
           <div class="col-lg-6 col-md-12">
-              <div class="contact-form pb_70">
+              <div class="contact-form">
                   <form action="{{ route('send_contact') }}" method="POST">
                     @csrf
                       <div class="mb-3">
@@ -36,7 +36,7 @@
                           <label for="" class="form-label">Message *</label>
                           <textarea class="form-control" rows="3" name="message"></textarea>
                       </div>
-                      <div class="mb-3">
+                      <div>
                           <button type="submit">
                               Send Message
                           </button>
@@ -46,7 +46,7 @@
           </div>
           <div class="col-lg-6 col-md-12">
               <div class="map">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.2799198932!2d-74.25987701513004!3d40.69767006272707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1645362221879!5m2!1sen!2sbd" width="600" height="450" style="border: 0" allowfullscreen="" loading="lazy"></iframe>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.2799198932!2d-74.25987701513004!3d40.69767006272707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1645362221879!5m2!1sen!2sbd"  style="border: 0" allowfullscreen="" loading="lazy"></iframe>
               </div>
           </div>
       </div>
